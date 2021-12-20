517187952280146817de68f1eb3cfbb90a100cc0
pottimurthijaswanth <93123286+pottimurthijaswanth@users.noreply.github.com>
2021-12-20 23:12:34 +0530

    Create Q4.js
    es6 and ts assignments

---
+++ Es6 and Typescript Assignments/AssignmentNo1/Q4.js
@@ -0,0 +1,15 @@
+
+// Arrow Function & Converting array string into Array Onject
+const convertArrayToObj = (arr) => {
+    let objectArray = [];
+    arr.forEach((element) => {
+        var tempObj = {
+            name: element, 
+            length: element.length
+        };
+        objectArray.push(tempObj);
+    });
+    return objectArray;
+} 
+let myArrayStr = ['Hello', 'How', 'sai', 'srpt', 'to'];
+convertArrayToObj(myArrayStr);
